41027a74b344048702b4c50292e52f44503822f0
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-12-11 14:25:17 +0000

    first commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,69 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import streamlit as st
-from streamlit.logger import get_logger
+import requests
+from pathlib import Path
 
-LOGGER = get_logger(__name__)
+#base_url = 'https://raw.githubusercontent.com/Hezel2000/cosmogeochemdata/master/'
 
+def upload_to_github(file_path, commit_message):
+    # Replace these variables with your GitHub repository details
+    repo_owner = "Hezel2000"
+    repo_name = "cosmogeochemdata"
+    branch_name = "main"
+    file_name = file_path.name
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+    # Get the content of the existing file on GitHub
+    github_api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_name}"
+    response = requests.get(github_api_url)
+
+    if response.status_code == 200:
+        existing_content = response.json()
+        sha = existing_content["sha"]
+    else:
+        existing_content = None
+        sha = None
+
+    # Read the local file content
+    with open(file_path, "rb") as file:
+        file_content = file.read()
+
+    # Create a new commit with the updated file
+    commit_data = {
+        "message": commit_message,
+        "content": file_content.decode("utf-8"),
+        "sha": sha,
+        "branch": branch_name
+    }
+
+    # Use your GitHub token here
+    github_token = "GitHub_Token"
+    response = requests.put(
+        github_api_url,
+        headers={"Authorization": f"Bearer {github_token}"},
+        json=commit_data
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    if response.status_code == 200:
+        st.success(f"File '{file_name}' uploaded to GitHub successfully!")
+    else:
+        st.error(f"Error uploading file to GitHub. Status Code: {response.status_code}, Response: {response.text}")
 
-    st.sidebar.success("Select a demo above.")
+st.title("GitHub File Uploader")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# File uploader widget
+uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "xlsx"])
 
+if uploaded_file is not None:
+    st.write("File uploaded successfully!")
 
-if __name__ == "__main__":
-    run()
+    # Save the uploaded file to the server in the uploads folder
+    file_path = Path("uploads") / uploaded_file.name
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success(f"File saved to {file_path}")
+
+    # Commit and push changes to GitHub
+    commit_message = st.text_input("Enter commit message:")
+    if st.button("Upload to GitHub"):
+        upload_to_github(file_path, commit_message)
