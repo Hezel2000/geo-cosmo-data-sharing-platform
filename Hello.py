b1ae53bf340a15d53b4fcdb7c667c84825efc3d7
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-12-30 12:03:13 +0000

    test

--- Hello.py
+++ Hello.py
@@ -8,74 +8,74 @@ import base64
 
 st.title('Will soon become functional again')
 
-st.stop()
+# st.stop()
 
-def upload_to_github(file_path, commit_message):
-    repo_owner = "Hezel2000"
-    repo_name = "GeoCosmoChemDataAndTools"
-    branch_name = "main"
-    file_name = file_path.name
+# def upload_to_github(file_path, commit_message):
+#     repo_owner = "Hezel2000"
+#     repo_name = "GeoCosmoChemDataAndTools"
+#     branch_name = "main"
+#     file_name = file_path.name
 
-    # Get the content of the existing file on GitHub
-    github_api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_name}"
-    response = requests.get(github_api_url)
+#     # Get the content of the existing file on GitHub
+#     github_api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_name}"
+#     response = requests.get(github_api_url)
     
-    if response.status_code == 200:
-        existing_content = response.json()
-        sha = existing_content["sha"]
-    else:
-        existing_content = None
-        sha = None
+#     if response.status_code == 200:
+#         existing_content = response.json()
+#         sha = existing_content["sha"]
+#     else:
+#         existing_content = None
+#         sha = None
 
-    # Read the local file content
-    with open(file_path, "rb") as file:
-        file_content = file.read()
-        base64_bytes = base64.b64encode(file_content) 
-        base64_string = base64_bytes.decode("utf-8")
-    # sample_string = "GeeksForGeeks is the best"
-    # sample_string_bytes = sample_string.encode("ascii") 
-    # print(f"Encoded string: {base64_string}") 
+#     # Read the local file content
+#     with open(file_path, "rb") as file:
+#         file_content = file.read()
+#         base64_bytes = base64.b64encode(file_content) 
+#         base64_string = base64_bytes.decode("utf-8")
+#     # sample_string = "GeeksForGeeks is the best"
+#     # sample_string_bytes = sample_string.encode("ascii") 
+#     # print(f"Encoded string: {base64_string}") 
 
-    # Create a new commit with the updated file
-    commit_data = {
-        "message": commit_message,
-        "content": base64_string,
-        "sha": sha,
-        "branch": branch_name
-    }
+#     # Create a new commit with the updated file
+#     commit_data = {
+#         "message": commit_message,
+#         "content": base64_string,
+#         "sha": sha,
+#         "branch": branch_name
+#     }
 
-    # Use your GitHub token here
-    github_token = st.secrets["GitHub_Token"]
-    response = requests.put(
-        github_api_url,
-        headers={"Authorization": f"Bearer {github_token}"},
-        json=commit_data
-    )
+#     # Use your GitHub token here
+#     github_token = st.secrets["GitHub_Token"]
+#     response = requests.put(
+#         github_api_url,
+#         headers={"Authorization": f"Bearer {github_token}"},
+#         json=commit_data
+#     )
 
-    return response
+#     return response
 
-st.title("GitHub File Uploader")
+# st.title("GitHub File Uploader")
 
-# File uploader widget
-uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "xlsx"])
+# # File uploader widget
+# uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "xlsx"])
 
-if uploaded_file is not None:
-    st.write("File uploaded successfully!")
+# if uploaded_file is not None:
+#     st.write("File uploaded successfully!")
 
-    # Save the uploaded file to the server in the uploads folder
-    file_path = Path("uploads") / uploaded_file.name
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+#     # Save the uploaded file to the server in the uploads folder
+#     file_path = Path("uploads") / uploaded_file.name
+#     with open(file_path, "wb") as f:
+#         f.write(uploaded_file.getbuffer())
 
-    st.success(f"File saved to {file_path}")
+#     st.success(f"File saved to {file_path}")
 
-    # Commit and push changes to GitHub
-    commit_message = st.text_input("Enter commit message:")
-    if st.button("Upload to GitHub"):
-        response = upload_to_github(file_path, commit_message)
+#     # Commit and push changes to GitHub
+#     commit_message = st.text_input("Enter commit message:")
+#     if st.button("Upload to GitHub"):
+#         response = upload_to_github(file_path, commit_message)
 
-        if response.status_code == 201:
-            st.success(f"File was was uploaded to GitHub successfully!")
-            #st.write(response.text.content.name)
-        else:
-            st.error(f"Error uploading file to GitHub. Status Code: {response.status_code}, Response: {response.text}")
+#         if response.status_code == 201:
+#             st.success(f"File was was uploaded to GitHub successfully!")
+#             #st.write(response.text.content.name)
+#         else:
+#             st.error(f"Error uploading file to GitHub. Status Code: {response.status_code}, Response: {response.text}")
