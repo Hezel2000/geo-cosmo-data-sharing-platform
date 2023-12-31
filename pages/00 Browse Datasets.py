84a4e549c3403653ae2cfc8018f137132cf7c252
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-12-31 08:56:21 +0000

    Browse Datasets corrected

--- pages/00 Browse Datasets.py
+++ pages/00 Browse Datasets.py
@@ -4,12 +4,6 @@ import pandas as pd
 
 st.title('Browse Datasets')
 
-#api_url = f'https://api.github.com/repos/Hezel2000/GeoCosmoChemDataAndTools/contents/csv?ref=main'
-#github_token = st.secrets["GitHub_Token"]
-#headers = {'Authorization': f'Bearer {github_token}'}
-#response = requests.get(api_url, headers=headers)
-#st.write(response.status_code)
-
 def get_github_folder_contents(username, repository, path, branch='main'):
     api_url = f'https://api.github.com/repos/{username}/{repository}/contents/{path}?ref={branch}'
     github_token = st.secrets['GitHub_Token']
@@ -28,8 +22,6 @@ repository = 'GeoCosmoChemDataAndTools'
 folder_path = 'csv'
 
 contents = get_github_folder_contents(username, repository, folder_path)
-st.write(username)
-st.write(contents)
 
 if contents:
     dataset_name_list = []
@@ -39,11 +31,6 @@ if contents:
             dataset_name_list.append(item['name'].split('.')[0])
             dataset_download_urls.append(item['download_url'])
 
-st.write(dataset_download_urls)
-
 sel_dataset = st.selectbox('Select Dataset', dataset_name_list)
 
 st.dataframe(pd.read_csv(dataset_download_urls[dataset_name_list.index(sel_dataset)]))
-
-
-# 
