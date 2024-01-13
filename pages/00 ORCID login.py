0675ec7f2f3ef51ec6f3a4bfcf1e2ea75550c1cc
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-01-13 17:27:40 +0100

    tet

--- pages/00 ORCID login.py
+++ pages/00 ORCID login.py
@@ -34,7 +34,7 @@ def get_orcid_user_info(orcid_token):
     headers = {"Authorization": f"Bearer {orcid_token}"}
     response = requests.get(ORCID_API_URL + "me", headers=headers)
 
-    if response.status_code() == 200:
+    if response.status_code == 200:
         user_info = response.json()
         return {
             "name": user_info.get("name", {}).get("given-names", {}).get("value", ""),
