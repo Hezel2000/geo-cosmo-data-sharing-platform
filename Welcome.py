c1bcc316553187369e5ad9458f06e0384e61f5f2
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-01-13 15:45:44 +0100

    test

--- Welcome.py
+++ Welcome.py
@@ -72,6 +72,7 @@ if not st.session_state.is_authenticated:
 # Display user info if authenticated
 if st.session_state.is_authenticated:
     st.sidebar.success("You are logged in with Orcid.")
+    st.sidebar.button('Logout', on_click)
 
     # Display Orcid user info automatically
     orcid_user_info = get_orcid_user_info(st.session_state.orcid_token)
@@ -83,3 +84,4 @@ if st.session_state.is_authenticated:
     # Your existing Streamlit content goes here
     st.title('Your uploaded files')
     st.write('A simply filtered table with your uploaded datasets, with a number of editing options: update, delete (restricted!)')
+    st.write(orcid_user_info)
