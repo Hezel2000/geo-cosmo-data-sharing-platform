41e2d0ba69254c7d095b333d4856661e10dd4fa2
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-01-16 16:20:04 +0100

    test

--- pages/06 user info.py
+++ pages/06 user info.py
@@ -18,7 +18,7 @@ def get_orcid_user_info(orcid_token):
             "orcid": user_info.get("orcid-identifier", {}).get("path", ""),
         }
     else:
-        return response.status_code
+        return ORCID_API_URL + "me"
 
 # Display user info if authenticated
 if st.session_state.is_authenticated:
