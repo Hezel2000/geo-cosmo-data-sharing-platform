54d64c160361dabf44b2fd25814b999a5630d8c7
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-01-16 13:42:56 +0100

    Orcid page renamed

--- pages/tmp.py
+++ pages/tmp.py
@@ -3,12 +3,12 @@ from requests.auth import HTTPBasicAuth
 import requests
 
 # Orcid Auth
-CLIENT_ID = st.secrets["Orcid_ID"]
-CLIENT_SECRET = st.secrets["Orcid_Secret"]
-# REDIRECT_URI = "https://orcid-app-u9tbyykcsuwozua46jf3hk.streamlit.app/"
-# REDIRECT_URI = "https://geo-cosmo-data-sharing-platform-bvniuih82j6l2aeq3jxfyb.streamlit.app/"
-REDIRECT_URI = "https://geo-cosmo-data-sharing-platform-bvniuih82j6l2aeq3jxfyb.streamlit.app/ORCID_login_&_logout"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+REDIRECT_URI = "https://orcid-app-u9tbyykcsuwozua46jf3hk.streamlit.app/"
 ORCID_API_URL = "https://pub.orcid.org/v3.0/"
+
+
 
 # Function to get Orcid token
 def get_orcid_token(authorization_response):
@@ -29,7 +29,7 @@ def get_orcid_token(authorization_response):
 # Function to get Orcid user info
 def get_orcid_user_info(orcid_token):
     if not orcid_token:
-        return st.error('no orcid token')
+        return None
 
     headers = {"Authorization": f"Bearer {orcid_token}"}
     response = requests.get(ORCID_API_URL + "me", headers=headers)
@@ -41,31 +41,31 @@ def get_orcid_user_info(orcid_token):
             "orcid": user_info.get("orcid-identifier", {}).get("path", ""),
         }
     else:
-        return st.error('info response error')
+        return None
 
 # Streamlit app
-st.title("ORCID Authentication")
+st.title("Streamlit Orcid Authentication")
 
-st.write('Works as follows (for the moment:)')
-st.write('Click on the button, click on the link – then authenticate.')
-st.write('After the redirect to this page, click again on login – and you are all set.')
-st.write('This will become more streamlined in the future.')
 
-st.write(' ')
-st.write('To logout – simply return to this page and you will be logged aout fro ORCID.')
+
+
+
+
+
+
 
 # Check if the user is authenticated
-st.session_state.is_authenticated = False #st.session_state.get("is_authenticated", False)
+is_authenticated = st.session_state.get("is_authenticated", False)
 
-if not st.session_state.is_authenticated:
+if not is_authenticated:
     # Orcid login button
-    if st.button("Login with ORCID"):
+    if st.button("Login with Orcid"):
         # Redirect user to Orcid for authorization
         authorization_url = f"https://orcid.org/oauth/authorize?client_id={CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri={REDIRECT_URI}"
         st.write(f"Click [here]({authorization_url}) to log in with Orcid.")
 
         # Check if the authorization code is present in the URL
-        url = st.experimental_get_query_params() # should soon be: st.query_params()
+        url = st.experimental_get_query_params()
         authorization_response = url.get("code", None)
 
         if authorization_response:
@@ -75,16 +75,14 @@ if not st.session_state.is_authenticated:
             if orcid_token:
                 st.session_state.is_authenticated = True
                 st.session_state.orcid_token = orcid_token
-                st.success("Successfully logged in with ORCID")
-
+                st.success("Successfully logged in with Orcid!")
 
 # Display user info if authenticated
-if st.session_state.is_authenticated:
-    st.sidebar.success("You are logged in with ORCID")
+if is_authenticated:
+    st.sidebar.info("You are logged in with Orcid.")
 
     # Display Orcid user info automatically
     orcid_user_info = get_orcid_user_info(st.session_state.orcid_token)
-    st.write('response.status_code', orcid_user_info)
     if orcid_user_info:
         st.write("Orcid User Information:")
         st.write(f"Name: {orcid_user_info['name']}")
@@ -93,7 +91,3 @@ if st.session_state.is_authenticated:
     # Your existing Streamlit content goes here
     st.title('Your uploaded files')
     st.write('A simply filtered table with your uploaded datasets, with a number of editing options: update, delete (restricted!)')
-    st.write('orcid_user_info', orcid_user_info)
-    
-else:
-    st.sidebar.error('You are not loged in to ORCID')
