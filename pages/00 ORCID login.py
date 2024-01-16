05d1ba80d22235fdd3aad4385ce939733690adc3
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-01-16 16:47:33 +0100

    test

--- pages/00 ORCID login.py
+++ pages/00 ORCID login.py
@@ -8,7 +8,7 @@ CLIENT_SECRET = st.secrets["Orcid_Secret"]
 # REDIRECT_URI = "https://orcid-app-u9tbyykcsuwozua46jf3hk.streamlit.app/"
 # REDIRECT_URI = "https://geo-cosmo-data-sharing-platform-bvniuih82j6l2aeq3jxfyb.streamlit.app/"
 REDIRECT_URI = "https://geo-cosmo-data-sharing-platform-bvniuih82j6l2aeq3jxfyb.streamlit.app/ORCID_login"
-ORCID_API_URL = "https://pub.orcid.org/v3.0/"
+# ORCID_API_URL = "https://pub.orcid.org/v3.0/"
 
 # Function to get Orcid token
 def get_orcid_token(authorization_response):
@@ -26,23 +26,6 @@ def get_orcid_token(authorization_response):
     else:
         return None
 
-# # Function to get Orcid user info
-# def get_orcid_user_info(orcid_token):
-#     if not orcid_token:
-#         return st.error('no orcid token')
-
-#     headers = {"Authorization": f"Bearer {orcid_token}"}
-#     response = requests.get(ORCID_API_URL + "me", headers=headers)
-
-#     if response.status_code == 200:
-#         user_info = response.json()
-#         return {
-#             "name": user_info.get("name", {}).get("given-names", {}).get("value", ""),
-#             "orcid": user_info.get("orcid-identifier", {}).get("path", ""),
-#         }
-#     else:
-#         return st.error('info response error')
-    
 
 # Function to get Orcid user info
 def get_orcid_user_info(orcid_token):
@@ -59,12 +42,13 @@ def get_orcid_user_info(orcid_token):
 
     if response.status_code == 200:
         user_info = response.json()
+        return response.text()
         return {
             "name": user_info.get("name", ""),
             "orcid": user_info.get("sub", ""),  # Using 'sub' as Orcid ID, you may need to adjust this based on the response format
         }
     else:
-        return None
+        return st.error('info response error')
 
 # Streamlit app
 st.title("ORCID Authentication")
